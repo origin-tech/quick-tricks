646bbdf0c5134d1e576b898b5125119dcc3fc213
Tech <135502534+origin-tech@users.noreply.github.com>
2023-12-25 02:03:27 +0000

    Update spoofing.go

--- cmd/spoofing.go
+++ cmd/spoofing.go
@@ -2,8 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/indigo-sadland/quick-tricks/modules/spoofing"
-	"github.com/indigo-sadland/quick-tricks/utils/colors"
+	"github.com/origin-tech/quick-tricks/modules/spoofing"
+	"github.com/origin-tech/quick-tricks/utils/colors"
 	"github.com/spf13/cobra"
 )
 

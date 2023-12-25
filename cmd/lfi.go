039e852f92f543ba715d6f456a344ab9caec8d7d
Tech <135502534+origin-tech@users.noreply.github.com>
2023-12-25 02:01:33 +0000

    Update lfi.go

--- cmd/lfi.go
+++ cmd/lfi.go
@@ -2,8 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/indigo-sadland/quick-tricks/modules/lfi"
-	"github.com/indigo-sadland/quick-tricks/utils/colors"
+	"github.com/origin-tech/quick-tricks/modules/lfi"
+	"github.com/origin-tech/quick-tricks/utils/colors"
 
 	"github.com/spf13/cobra"
 )

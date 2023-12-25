2fc34cf7feb101264baa875daec7175a87b25963
Tech <135502534+origin-tech@users.noreply.github.com>
2023-12-25 02:02:27 +0000

    Update recon.go

--- cmd/recon.go
+++ cmd/recon.go
@@ -3,10 +3,10 @@ package cmd
 import (
 	"fmt"
 	"github.com/fatih/color"
-	"github.com/indigo-sadland/quick-tricks/modules/recon/license"
-	"github.com/indigo-sadland/quick-tricks/modules/recon/lp"
-	"github.com/indigo-sadland/quick-tricks/modules/recon/lpd"
-	"github.com/indigo-sadland/quick-tricks/utils/colors"
+	"github.com/origin-tech/quick-tricks/modules/recon/license"
+	"github.com/origin-tech/quick-tricks/modules/recon/lp"
+	"github.com/origin-tech/quick-tricks/modules/recon/lpd"
+	"github.com/origin-tech/quick-tricks/utils/colors"
 	"github.com/spf13/cobra"
 	"strings"
 )
